49109a5422614ee8fa5d0e600b37721cf35b83ec
zerobell-lee <bnb3456@gmail.com>
2019-01-20 22:07:16 +0900

    Async Await

--- src/App.js
+++ src/App.js
@@ -9,19 +9,30 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating')
-    .then(res => res.json())
-    .then(json => console.log(json))
-    .catch(err => console.log(err))
+    this._getMovies();
   }
 
   _renderMovies = () => {
     const movies = this.state.movies.map((movie, index) => {
-      return <Movie title={movie.title} poster={movie.poster} key={index} />
+      return <Movie title={movie.title} poster={movie.large_cover_image} key={index} />
     });
 
     return movies;
   };
+
+_getMovies = async () => {
+  const movies = await this._callApi()
+  this.setState({
+    movies
+  })
+}
+
+_callApi = () => {
+  return fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating')
+    .then(res => res.json())
+    .then(json => json.data.movies)
+    .catch(err => console.log(err))
+}
 
   render() {
     return (
